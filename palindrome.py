51d0bae10f8a8753b7f4d948bf7478bc684fb05e
Rob Taylor <rob.taylor543@gmail.com>
2019-02-20 11:01:05 -0500

    Simplified recursive program

--- palindrome.py
+++ palindrome.py
@@ -13,7 +13,7 @@ def palindrome(input_string):
 def palindrome_recursively(input_string):
     """Takes a string as an argument, cleans it of punctuation, spaces, and special characters, and returns True if it is a palindrome and False otherwise."""
     cleaned_input_string = clean_text(input_string)
-    return (len(cleaned_input_string) <= 1) or ((cleaned_input_string.endswith(cleaned_input_string[0])) and palindrome_recursively(cleaned_input_string[1:len(cleaned_input_string)-1]))
+    return (len(cleaned_input_string) <= 1) or ((cleaned_input_string.endswith(cleaned_input_string[0])) and palindrome_recursively(cleaned_input_string[1:-1]))
 
 
 user_input = input("Write something to find out if it is a palindrome: ")
